809d905cbccd2ab65ffcda7f2b4472dbc4a8866b
Arthur Martins <arthurlopesmartins@hotmail.com>
2025-07-22 10:57:50 -0300

    feat: changing the text font to roboto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Roboto } from "next/font/google";
 import "./globals.css";
+
+const roboto = Roboto({
+  weight: ['400', '500', '700'], // Escolha os pesos que você usa (regular, medium, bold)
+  subsets: ['latin'], // Geralmente 'latin' para português
+  display: 'swap', // Garante que o texto seja visível durante o carregamento da fonte
+  variable: '--font-roboto', // Opcional: para usar como variável CSS
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${roboto.className} ${roboto.variable}`}
       >
         {children}
       </body>
