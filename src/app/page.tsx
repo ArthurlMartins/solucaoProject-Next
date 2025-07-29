cd1e2acfbec01085892b6d53e07dfdb6e1009883
Arthur Martins <arthurlopesmartins@hotmail.com>
2025-07-29 10:27:25 -0300

    feat: section objective

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+"use client"
+
 import About from "@/components/AboutSection";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
-import Objective from "@/components/Objective";
+import Objective from "@/components/ObjectiveSection";
 import ServicesSection from "@/components/ServicesSection";
 import UnderFooter from "@/components/UnderFooter";
+import { ParallaxProvider } from 'react-scroll-parallax';
 import Image from "next/image";
 
 export default function Home() {
@@ -13,12 +16,16 @@ export default function Home() {
        <Header />
         <main>
           <HeroSection />
-          <Objective />
+          <ParallaxProvider>
+            <Objective />
+          </ParallaxProvider>
           <ServicesSection />
           <About />
+          
         </main>
        <Footer />
        <UnderFooter />
+       
       </>
   );
 }
