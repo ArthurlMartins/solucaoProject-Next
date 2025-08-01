76a5d5122227227bfb9a43ecff872411c17f931d
Arthur Martins <arthurlopesmartins@hotmail.com>
2025-07-31 22:26:49 -0300

    feat: footer done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,78 @@
+
+
 export default function Footer() {
     return(
         <>
-            
+            <div className="w-full bg-black">
+                <div className="container pt-[60px] pb-[60px]">
+                    <div className="grid md:grid-cols-2 text-white place-content-between">
+
+                        <div className="">
+                            <p className="text-2xl font-bold">Empresa Solução</p>
+                            <p className="text-sm">aempresasolucao@gmail.com</p>
+                            <p className="text-sm">51 984182894</p>
+                        </div>
+                        
+                        <div className="grid lg:grid-cols-2 mt-[200px] md:mt-0">
+                            <div>
+                                <p className="text-lg font-bold mb-4">Locais e contatos de referência</p>
+                                <div className="flex-col space-y-6 max-w-[250px]">
+                                    <div className="text-xs">
+                                        <p className=" font-bold">Templo de Porto Alegre</p>
+                                        <p>(51) 99316-5344 Lidson (Engenharia)</p>
+                                        <p>bassanils@churchofjesuschrist.org</p>
+                                    </div>
+                                    <div className="text-xs">
+                                        <p className=" font-bold">Associação Brasileira A Igreja de Jesus Cristodos S.U.D – Regiãode Porto Alegre
+                                        </p>
+                                        <p>(51) 99316-5344 Lidson (Engenharia)</p>
+                                        <p>bassanils@churchofjesuschrist.org</p>
+                                    </div>
+                                    <div className="text-xs">
+                                        <p className=" font-bold">Templo de Porto Alegre</p>
+                                        <p>(51) 99316-5344 Lidson (Engenharia)</p>
+                                        <p>bassanils@churchofjesuschrist.org</p>
+                                    </div>
+                                    <div className="text-xs">
+                                        <p className=" font-bold">Templo de Porto Alegre</p>
+                                        <p>(51) 99316-5344 Lidson (Engenharia)</p>
+                                        <p>bassanils@churchofjesuschrist.org</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div>
+                                <p className="text-xl font-bold mb-4"></p>
+                                <div className="flex-col space-y-6  max-w-[250px]">
+                                    <div className="text-xs">
+                                        <p className=" font-bold">Templo de Porto Alegre</p>
+                                        <p>(51) 99316-5344 Lidson (Engenharia)</p>
+                                        <p>bassanils@churchofjesuschrist.org</p>
+                                    </div>
+                                    <div className="text-xs">
+                                        <p className=" font-bold">Associação Brasileira A Igreja de Jesus Cristodos S.U.D – Regiãode Porto Alegre
+                                        </p>
+                                        <p>(51) 99316-5344 Lidson (Engenharia)</p>
+                                        <p>bassanils@churchofjesuschrist.org</p>
+                                    </div>
+                                    <div className="text-xs">
+                                        <p className=" font-bold">Templo de Porto Alegre</p>
+                                        <p>(51) 99316-5344 Lidson (Engenharia)</p>
+                                        <p>bassanils@churchofjesuschrist.org</p>
+                                    </div>
+                                    <div className="text-xs">
+                                        <p className=" font-bold">Templo de Porto Alegre</p>
+                                        <p>(51) 99316-5344 Lidson (Engenharia)</p>
+                                        <p>bassanils@churchofjesuschrist.org</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
